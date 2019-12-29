3cf372c0439e1478e428a1125bc46c17b6565017
Sam Robbins <samrobbinsgb@gmail.com>
2019-12-29 17:57:58 +0000

    Fix navbar

--- themes/kross-hugo-portfolio-template/assets/js/script.js
+++ themes/kross-hugo-portfolio-template/assets/js/script.js
@@ -1,7 +1,6 @@
 (function ($) {
     'use strict';
 
-
     // Preloader js    
     $(window).on('load', function () {
         $('.preloader').fadeOut(100);
@@ -113,3 +112,18 @@
 
 })(jQuery);
 
+
+// Get the container element
+var btnContainer = document.getElementById("navigation");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("a");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
