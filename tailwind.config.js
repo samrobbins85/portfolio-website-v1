e0999e0ce1175fb01854c3101ecb76dbbfb04232
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-31 15:30:49 +0100

    Start work converting to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    content: [
+      "./layouts/**/*.html",
+      "./content/**/*.md",
+      "./content/**/*.html",
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
